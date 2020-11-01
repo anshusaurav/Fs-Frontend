59f001dba4b4db001dc45b140f1f90dc55a30003
anshusaurav <anshu.saurav@gmail.com>
2020-11-01 14:15:53 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./App.css";
+import Upload from "./Components/Upload"
+import "react-toastify/dist/ReactToastify.css";
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Upload />
+      </div>
+    );
+  }
+}
+
+export default App;
